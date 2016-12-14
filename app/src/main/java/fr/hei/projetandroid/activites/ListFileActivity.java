63925360ac869132f31c0efc06a0d4c5fbe059de
makidelille <makidelille@bitbucket.org>
2016-12-14 14:29:46 +0100

    made fileexplorer

--- app/src/main/java/fr/hei/projetandroid/activites/ListFileActivity.java
+++ app/src/main/java/fr/hei/projetandroid/activites/ListFileActivity.java
@@ -2,13 +2,13 @@ package fr.hei.projetandroid.activites;
 
 import android.app.ListActivity;
 import android.content.Intent;
-import android.graphics.Path;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 import fr.hei.projetandroid.R;
-import fr.hei.projetandroid.activites.adapters.FileAdapter;
-import fr.hei.projetandroid.utils.FileItem;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListFileActivity extends ListActivity {
 
@@ -17,29 +17,29 @@ public class ListFileActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
 
         Intent it = getIntent();
         String path  = it.hasExtra(EXTRA_PATH) ? it.getStringExtra(EXTRA_PATH) :  "/";
         File f = new File(path);
 
         if(f.canRead()){
-            FileItem[] files = getFiles(f);
-            this.setListAdapter(new FileAdapter(this,R.layout.fragment_listview,files));
-            //robz
-            new String[] {"img", "nom", "taille"}, new int[] {R.id.img, R.id.txtnom, R.id.txttaille,});
+            ArrayList<HashMap<String,String>> files = getFiles(f);
+            this.setListAdapter(new SimpleAdapter(this,files,R.layout.fragment_listview,new String[] {"img", "nom", "taille"}, new int[] {R.id.img, R.id.txtnom, R.id.txttaille,}));
             //robz
         }
 
     }
 
-    private FileItem[] getFiles(File f) {
+    private ArrayList<HashMap<String,String>> getFiles(File f) {
         File[] childs = f.listFiles();
-        FileItem[] ret = new FileItem[childs.length];
-        for(int i=0; i< ret.length; i++){
-            ret[i] = new FileItem(childs[i].getName(),childs[i].length(),childs[i].isDirectory());
+        ArrayList<HashMap<String,String>> ret = new ArrayList<>();
+        for(int i=0; i< childs.length; i++){
+            HashMap<String,String> fichier = new HashMap<>();
+            fichier.put("img", String.valueOf(childs[i].isDirectory() ? R.drawable.dossieric : R.drawable.fichieric));
+            fichier.put("nom",childs[i].getName());
+            fichier.put("taille", String.valueOf(childs[i].length()));
+            ret.add(fichier);
         }
-
         return ret;
 
     }
